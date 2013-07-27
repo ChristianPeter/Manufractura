dff7d759ff7deb30fdbfe1bbe2717d6c949b4339
Christian Peter <peter.chr@web.de>
2013-07-27 12:43:42 +0200

    initial commit

---
+++ public_html/js/fracWorker.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var width;
+var height;
+var imgData;
+
+
+
+function calculate(){
+    
+}
+
+function messageHandler(event){
+    
+}
+
+this.addEventListener('message', messageHandler, false);
